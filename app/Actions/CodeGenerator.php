17ceece08483e27afe8444eaf79af653a18ee4b7
Bruno Bastos <brunomauro@gmail.com>
2022-02-28 16:16:08 -0300

    Cria um construtor em App\Actions\CodeGenerator para que o length do "code" seja configurável.

--- app/Actions/CodeGenerator.php
+++ app/Actions/CodeGenerator.php
@@ -7,9 +7,17 @@ use Illuminate\Support\Str;
 
 class CodeGenerator
 {
+    //definindo um construtor para chamar a classo passando length diferente nos testes por exemplo
+    public function __construct(
+        private int $length = 0
+    )
+    {
+        $this->length = $length ?? config('app.short_url_code_length');
+    }
+
     public function run(): string
     {
-        $code = Str::random(5);
+        $code = Str::random($this->length);
 
         return ShortUrl::where('code', $code)->count()
             ? $this->run()
